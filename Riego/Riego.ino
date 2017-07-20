0a02c421bc1ec2bea2e31cda9f3ed8bfb7116650
Jesús <jcsombria@gmail.com>
2017-07-20 15:15:46 +0200

    Añadido código Arduino

--- Riego/Riego.ino
+++ Riego/Riego.ino
@@ -2,21 +2,19 @@
 #include <TimerOne.h>
 #include "control.h"
 
-// Sensor
 const int period = 10;
 
 const int pinHumedad = A1;
 const int pinTemperatura = A0;
-
 const int pinPump = 3;
 
-Sensor T0, H0;
 SoftwareSerial USB(0, 1), BT(10, 11); // RX | TX
-
+Sensor T0, H0;
 Actuator P0;
 
 int time = 0;
 int last = 0;
+
 // Control Loop
 void tic() {
 	time ++;
@@ -34,15 +32,13 @@ double volts2humidity(double volts) {
 double temperatura = 0;
 double humedad = 0;
 boolean nuevo = false;
-//const double kv = 5.0 / 1024.0; 
 void read() {
-	double rawTemp = T0.read(),
-		rawHumid = H0.read();
-  temperatura = T0.convert(rawTemp);
-  humedad = H0.convert(rawHumid);
+	double rawTemp = T0.read(), rawHumid = H0.read();
+	temperatura = T0.convert(rawTemp);
+	humedad = H0.convert(rawHumid);
 	T0.update();
 	H0.update();
-  nuevo = true;
+	nuevo = true;
 }
 
 void setup() {
@@ -50,12 +46,12 @@ void setup() {
 	H0.setConvertFunction(volts2humidity);
 	T0.setPin(pinTemperatura);
 	T0.setConvertFunction(volts2celsius);
-  P0.setPin(pinPump);
-  P0.setRange(0.0, 0.1);
-  Timer1.initialize(1000000);
-  Timer1.attachInterrupt(tic);
-  BT.begin(9600);
-  USB.begin(9600);
+	P0.setPin(pinPump);
+	P0.setRange(0.0, 0.1);
+	Timer1.initialize(1000000);
+	Timer1.attachInterrupt(tic);
+	BT.begin(9600);
+	USB.begin(9600);
 }
 
 double pump = 0;
@@ -63,15 +59,15 @@ int nextTs = period;
 void loop() {
 	if(time > nextTs) {
 		nextTs += period;
-    nuevo = false;
-    logTo(USB);
-    logTo(BT);
-    P0.write(pump);
-    pump += 0.1;
-    if(pump > 1.0) {
-    	pump = 0.0;
-    }
-  }
+		nuevo = false;
+		logTo(USB);
+		logTo(BT);
+		P0.write(pump);
+		pump += 0.1;
+		if(pump > 1.0) {
+			pump = 0.0;
+		}
+	}
 }
 
 void logTo(SoftwareSerial dst) {
